b9507f6c997410c664d423526b495eedb5e6d990
Pranay dutta <rick15176@gmail.com>
2024-02-10 00:53:54 +0530

    working on mobile-ui

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,33 +1,73 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./buttons.css";
 import { themeContext } from "../../context/AllContext";
 import { randomThemeName, changeLineColor } from "./button";
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from "@emotion/react";
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+
 
 const Buttons = () => {
   const [currentTheme, setCurrentTheme] = useContext(themeContext);
+  const [windowWidth, setWindowWidth] = useState(null);
+
 
   const changeTheme = async () => {
     const [randomName, randomNumber] = await randomThemeName();
     setCurrentTheme(randomName);
     changeLineColor(randomNumber);
   };
-  return (
-    <div className="nav_btn_container">
-      <a method="POST" href="/">
-        <button className="btn btn-primary">Restart</button>
-      </a>
+  const getWidth = () => {
+    setWindowWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", getWidth);
+    //cleanup function
+    return () => {
+      window.removeEventListener("resize", getWidth);
+    }
+  })
+  const theme = createTheme({
+    palette: {
+      ochre: {
+        main: '#fff',
+        light: '#E9DB5D',
+        dark: '#A29415',
+        contrastText: '#242105',
+      },
+    },
+  });
 
-      <button
-        id="theme"
-        className="theme btn"
-        onClick={() => {
-          changeTheme();
-        }}
-      >
-        Change Theme
-      </button>
-      <p className="current_theme_name">Theme: {currentTheme}</p>
-    </div>
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="nav_btn_container">
+        {
+          windowWidth > 768
+            ?
+            <button
+              onClick={() => window.location.reload()}
+              className="btn btn-primary">
+              Restart
+            </button>
+            :
+            <RestartAltIcon
+              fontSize="large"
+              onClick={() => window.location.reload()}
+              sx={{ color: "ochre.main" }}
+            />
+        }
+        <button
+          id="theme"
+          className="theme btn"
+          onClick={() => {
+            changeTheme();
+          }}
+        >
+          Change Theme
+        </button>
+        <p className="current_theme_name">Theme: {currentTheme}</p>
+      </div>
+    </ThemeProvider>
   );
 };
 export default Buttons;
