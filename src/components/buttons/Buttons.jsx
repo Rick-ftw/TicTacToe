51c1662e3c5ca980e61cd787616acd6be2e7f858
Pranay dutta <rick15176@gmail.com>
2024-11-16 14:25:31 +0530

    Revert "fixed color issue with restart and changeTheeme button in portrait mode"
    This reverts commit 59291962829c6742e63e332dfe4b728f9076291a.

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -2,22 +2,27 @@ import { useContext, useEffect, useState } from "react";
 import "./buttons.css";
 import { themeContext } from "../../context/AllContext";
 import { randomThemeName, changeLineColor } from "./button";
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from "@emotion/react";
-import RestartAltIcon from "@mui/icons-material/RestartAlt";
-import TrendingFlatIcon from "@mui/icons-material/TrendingFlat";
-import Button from "@mui/material/Button";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Fade from "@mui/material/Fade";
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Fade from '@mui/material/Fade';
+
+
+
+
 
 const Buttons = () => {
   const [currentTheme, setCurrentTheme] = useContext(themeContext);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [anchorEl, setAnchorEl] = useState(null);
-  const [menuHeading, setMenuHeading] = useState("Choose Mode");
-  const [menuColor, setMenuColor] = useState("transparent");
+  const [menuHeading, setMenuHeading] = useState("Choose Mode")
+  const [menuColor, setMenuColor] = useState("transparent")
   const open = Boolean(anchorEl);
+
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -28,11 +33,11 @@ const Buttons = () => {
     if (value == "Easy") {
       setMenuColor("green");
     } else if (value == "Hard") {
-      setMenuColor("orange");
+      setMenuColor("orange")
     } else if (value == "Impossible") {
-      setMenuColor("red");
+      setMenuColor("red")
     } else {
-      setMenuColor("transparent");
+      setMenuColor("transparent")
     }
     setAnchorEl(null);
   };
@@ -44,17 +49,20 @@ const Buttons = () => {
   };
   const setWidth = () => {
     setWindowWidth(window.innerWidth);
-  };
+  }
   useEffect(() => {
     window.addEventListener("resize", setWidth);
     return () => {
       window.removeEventListener("resize", setWidth);
-    };
-  });
+    }
+  })
   const theme = createTheme({
     palette: {
-      secondaryColor: {
-        main: "var(--secondary-color)",
+      ochre: {
+        main: '#fff',
+        light: '#E9DB5D',
+        dark: '#A29415',
+        contrastText: '#242105',
       },
     },
   });
@@ -62,79 +70,73 @@ const Buttons = () => {
   return (
     <ThemeProvider theme={theme}>
       <div className="nav_btn_container">
-        {windowWidth < 768 ? (
-          <>
-            <RestartAltIcon
-              fontSize="large"
-              onClick={() => window.location.reload()}
-              sx={{ color: "secondaryColor.main"}}
-            />
-            <TrendingFlatIcon
-              fontSize="large"
-              className="theme"
-              sx={{ color: "secondaryColor.main" }}
-              onClick={() => {
-                changeTheme();
-              }}
-            />
-          </>
-        ) : (
-          <>
-            <button
-              onClick={() => window.location.reload()}
-              className="btn btn-primary"
-              color="var(--secondary-color"
-            >
-              Restart
-            </button>
-            <button
-              id="theme"
-              className="theme btn"
-              onClick={() => {
-                changeTheme();
-              }}
-            >
-              Change Theme
-            </button>
+        {
+          windowWidth < 768
+            ?
+            <>
+              <RestartAltIcon
+                fontSize="large"
+                onClick={() => window.location.reload()}
+                sx={{ color: "ochre.main" }}
+              />
+              <TrendingFlatIcon
+                fontSize="large"
+                className="theme"
+                sx={{ color: "ochre.main" }}
+                onClick={() => {
+                  changeTheme();
+                }}
+              />
+            </>
+            :
+            <>
 
-            <Button
-              id="fade-button"
-              aria-controls={open ? "fade-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              sx={{ color: "white", backgroundColor: menuColor }}
-              onClick={handleClick}
-            >
-              {menuHeading}
-            </Button>
-            <Menu
-              id="fade-menu"
-              MenuListProps={{
-                "aria-labelledby": "fade-button",
-              }}
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              TransitionComponent={Fade}
-            >
-              <MenuItem name="Easy" onClick={(e) => handleClose(e)}>
-                Easy
-              </MenuItem>
-              <MenuItem name="Hard" onClick={(e) => handleClose(e)}>
-                Hard
-              </MenuItem>
-              <MenuItem name="Impossible" onClick={(e) => handleClose(e)}>
-                Impossible
-              </MenuItem>
-            </Menu>
-          </>
-        )}
-        <p className="current_theme_name" style={{ opacity: "0%" }}>
-          {windowWidth > 768 ? currentTheme : ""}
-        </p>
+              <button
+                onClick={() => window.location.reload()}
+                className="btn btn-primary">
+                Restart
+              </button>
+              <button
+                id="theme"
+                className="theme btn"
+                onClick={() => {
+                  changeTheme();
+                }}
+              >
+                Change Theme
+              </button>
+
+              <Button
+                id="fade-button"
+                aria-controls={open ? 'fade-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                sx={{ color: "white", backgroundColor: menuColor }}
+                onClick={handleClick}
+
+              >
+                {menuHeading}
+              </Button>
+              <Menu
+                id="fade-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'fade-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                TransitionComponent={Fade}
+              >
+                <MenuItem name="Easy" onClick={(e) => handleClose(e)}>Easy</MenuItem>
+                <MenuItem name="Hard" onClick={(e) => handleClose(e)}>Hard</MenuItem>
+                <MenuItem name="Impossible" onClick={(e) => handleClose(e)}>Impossible</MenuItem>
+              </Menu>
+            </>
+        }
+        <p className="current_theme_name" style={{ opacity: "0%" }}>{windowWidth > 768 ? currentTheme : ""}</p>
       </div>
     </ThemeProvider>
   );
 };
 
-export default Buttons;
+export default Buttons
